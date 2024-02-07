5ad34cf1c8f4e4a1b028794e4babbd562838c79a
Anouar Haidary <anouar.haidary@gmail.com>
2024-02-07 19:30:57 +0100

    Pushing the server's code

---
+++ eulogy_server/index.js
@@ -0,0 +1,16 @@
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+
+// // mongoose.connect(process.env.MONGO_URI);
+
+// app.get('/', (req, res) => {
+//   res.send(`Server is running on port: ${port}`);
+// });
+
+// const port = process.env.PORT || 8080;
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
